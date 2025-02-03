b1e17a73b844acf002dc0bbb429537ee1be90788
Kotaro7750 <7750koutarou@gmail.com>
2025-02-03 21:19:07 +0900

    Fix: Partially satisfy requirements for installable PWA
    https://vite-pwa-org.netlify.app/guide/pwa-minimal-requirements.html#icons-images

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,24 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
+      manifest: {
+        name: 'Kontrollroum',
+        short_name: 'Kontrollroum',
+        description: 'Kontrollroum',
+        theme_color: '#000000',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      },
       devOptions: {
         enabled: true
       }
